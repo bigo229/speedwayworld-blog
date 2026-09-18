c2582c0b1b799b3798ea75de800283378d02fbef
bigo229 <bigo229@yahoo.com>
2026-09-18 13:46:12 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import path from 'path';
 
-// https://astro.build/config
+// https://astro.build
 export default defineConfig({
-	site: 'https://pages.dev', // Ensure this matches your production Pages URL!
+	site: 'https://speedwayworld-blog.bigo229.workers.dev/',
 	integrations: [
 		mdx(), 
 		sitemap({
 			filter: (page) => !page.includes('/admin')
 		})
-	]
+	],
+	vite: {
+		resolve: {
+			alias: {
+				'@components': path.resolve('./src/components')
+			}
+		}
+	}
 });
