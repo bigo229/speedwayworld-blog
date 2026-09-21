89020bae393e851c52cad88cdbe904268ed13da2
bigo229 <bigo229@yahoo.com>
2026-09-21 17:48:28 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE_LINE1 = 'SpeedwayWorld';
+export const SITE_TITLE_LINE2 = 'the latest speedway world news';
 export const SITE_DESCRIPTION = 'My Speedwayworld Blog - the latest speedway world news!!';
+
+// Add this line below to fix the Cloudflare build error:
+export const SITE_TITLE = `${SITE_TITLE_LINE1} | ${SITE_TITLE_LINE2}`;
