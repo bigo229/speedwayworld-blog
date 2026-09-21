4d13b7b3ab9acded8522af0c05dc24fb9deaa9d5
bigo229 <bigo229@yahoo.com>
2026-09-21 17:15:11 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SpeedwayWorld - the latest speedway world news';
+export const SITE_TITLE_LINE1 = 'SpeedwayWorld';
+export const SITE_TITLE_LINE2 = 'the latest speedway world news';
 export const SITE_DESCRIPTION = 'My Speedwayworld Blog - the latest speedway world news!!';
